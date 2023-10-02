d7c75c8f633ca361a47a363408dae5cbea172e39
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-02 17:07:58 -0300

    Criando página do Cadastro de Nova Categoria

---
+++ src/pages/NovaCategoria/index.js
@@ -0,0 +1,11 @@
+import FormularioNovaCategoria from 'components/FormularioNovaCategoria';
+import styles from './NovaCategoria.module.css'
+
+function NovaCategoria(){
+    
+    return(
+        <FormularioNovaCategoria />
+    )
+}
+
+export default NovaCategoria;
