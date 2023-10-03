faffb9123bf5deb4294885f7cf68202d4a582597
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:29:36 -0300

    Adicionando videosFiltrados.map ao Slider

--- src/components/Carrossel/Sliders/index.js
+++ src/components/Carrossel/Sliders/index.js
@@ -3,40 +3,37 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import VideoCard from '../VideoCard';
+import { useVideosContext } from 'context/Videos';
 
-function Sliders() {
+function Sliders ({videosFiltrados}) {
+  
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3
-      };
+        slidesToScroll: 3,
+        infinite: false,
+    };
 
     return (
+        
         <div className={styles.slider}>
             <Slider {...settings}>
-                <div>
-                    <VideoCard />
-                </div>
-                <div>
-                    <VideoCard />
-                </div>
-                <div>
-                    <VideoCard />
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
+                {videosFiltrados.map((video) => (
+                    <div key={video.id} className={styles.card}>
+                        <VideoCard
+                            id={video.id}
+                            capa={video.linkImagem}
+                            descricao={video.descricao}
+                            link={video.linkVideo}
+                        />
+                    </div>
+                ))}
             </Slider>
         </div>
     )
+
 }
 
 export default Sliders; 
