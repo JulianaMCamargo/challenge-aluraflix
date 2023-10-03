c4b54aebef85f7bbaa7c8140af4167471f3e12ae
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:24:33 -0300

    Configurando styles do button

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -10,14 +10,10 @@ function Cabecalho() {
     palette: {
       primary: {
         main: '#DB202C',
-        // light: will be calculated from palette.primary.main,
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
       },
       secondary: {
         main: '#DB202C',
         light: '#F5EBFF',
-        // dark: will be calculated from palette.secondary.main,
         contrastText: '#47008F',
       },
     },
@@ -25,11 +21,13 @@ function Cabecalho() {
 
   return (
     <header className={styles.cabecalho}>
+
       <Link to='/'>
         <img src={logo} alt='Logo do Leandroflix' />
       </Link>
-      <span style={{ cursor: 'not-allowed' }}>
-        <Button component={Link} variant="outlined" color='primary'> 
+
+      <Link to='/novo-video'>
+        <Button variant="outlined" color='primary'>
           Novo vídeo
         </Button>
       </Link>
