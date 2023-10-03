90d96917fa00eb23825f93acae68af13001b32bc
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:33:41 -0300

    Automatizando a criação de carrossel por categoria

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,13 +1,24 @@
+import Carrossel from 'components/Carrossel/Carrossel';
 import styles from './Inicio.module.css'
 import Banner from 'components/Banner';
-import Carrossel from 'components/Carrossel/Carrossel';
-
+import { useCategoriasContext } from 'context/Categorias';
 
 function Inicio() {
+    const {categorias} = useCategoriasContext();
+
     return (
         <>
             <Banner />
-            <Carrossel />
+            <section className={styles.container}>
+                {categorias.map((categoria) => (
+                    <Carrossel
+                        titulo={categoria.nome}
+                        cor={categoria.cor}
+                        descricao={categoria.descricao}
+                        key={categoria.id}
+                    />
+                ))}
+            </section>
         </>
     )
 }
