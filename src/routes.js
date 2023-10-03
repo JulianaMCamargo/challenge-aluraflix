7d3ed3d519cbdd590767f9ac510f3f0110ed7797
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:31:36 -0300

    Adicionando rota para página Nova Categoria

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import Inicio from "pages/Inicio";
+import NovaCategoria from "pages/NovaCategoria";
 import NovoVideo from "pages/NovoVideo";
 import PaginaPadrao from "pages/PaginaPadrao";
 
@@ -6,12 +7,14 @@ import PaginaPadrao from "pages/PaginaPadrao";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="novo-video" element={<NovoVideo />} />
+          <Route path="nova-categoria" element={<NovaCategoria />} />
         </Route>
       </Routes>
     </BrowserRouter>
