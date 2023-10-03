da2205ad0f715a82d25518ccdd1c00b72a266fb5
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:32:26 -0300

    Adicionando os Providers dos context

--- src/pages/PaginaPadrao/index.js
+++ src/pages/PaginaPadrao/index.js
@@ -1,12 +1,22 @@
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import CategoriasProvider from "context/Categorias";
+import ValidacoesProvider from "context/Validacoes";
+import VideosProvider from "context/Videos";
 import { Outlet } from "react-router-dom";
 
 function PaginaPadrao() {
+
     return (
         <>
             <Cabecalho />
-            <Outlet />
+            <ValidacoesProvider>
+                <VideosProvider>
+                    <CategoriasProvider>
+                        <Outlet />
+                    </CategoriasProvider>
+                </VideosProvider>
+            </ValidacoesProvider>
             <Rodape />
         </>
 
