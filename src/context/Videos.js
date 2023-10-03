f6f2a0a55c47bc2514c3d843d3f4c8428adfe181
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-03 15:35:02 -0300

    Conectando API

--- src/context/Videos.js
+++ src/context/Videos.js
@@ -1,53 +1,85 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export const VideosContext = createContext();
 VideosContext.displayName = 'Videos';
 
 
-    const inicial = [
-        {
-            id: uuidv4(),
-            titulo: "O labirinto do Logcat",
-            linkVideo: "https://www.youtube.com/embed/ypvGqZGJBls",
-            linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img2.png",
-            categoria: "Data Science",
-            descricao: "Qualquer coisa"
-        },
-        {
-            id: uuidv4(),
-            titulo: "Um componente no espaço",
-            linkVideo: "https://www.youtube.com/embed/jK0uiQ1ZQQQ",
-            linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img1.png",
-            categoria: "Mobile",
-            descricao: "Qualquer coisa"
-        },
-        {
-            id: uuidv4(),
-            titulo: "O poderoso JavaScript",
-            linkVideo: "https://www.youtube.com/embed/N1BSG02z-Mw",
-            linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img4.png",
-            categoria: "Devops",
-            descricao: "Qualquer coisa"
-        },
-        {
-            id: uuidv4(),
-            titulo: "Dart, o Caçador de Andróides",
-            linkVideo: "https://www.youtube.com/embed/LLmBWjF6F8M",
-            linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img3.png",
-            categoria: "Data Science",
-            descricao: "Qualquer coisa"
-        }
-    ]
+const inicial = [
+    //     {
+    //         id: uuidv4(),
+    //         titulo: "O labirinto do Logcat",
+    //         linkVideo: "https://www.youtube.com/embed/ypvGqZGJBls",
+    //         linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img2.png",
+    //         categoria: "Data Science",
+    //         descricao: "Qualquer coisa"
+    //     },
+    //     {
+    //         id: uuidv4(),
+    //         titulo: "Um componente no espaço",
+    //         linkVideo: "https://www.youtube.com/embed/jK0uiQ1ZQQQ",
+    //         linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img1.png",
+    //         categoria: "Mobile",
+    //         descricao: "Qualquer coisa"
+    //     },
+    //     {
+    //         id: uuidv4(),
+    //         titulo: "O poderoso JavaScript",
+    //         linkVideo: "https://www.youtube.com/embed/N1BSG02z-Mw",
+    //         linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img4.png",
+    //         categoria: "Devops",
+    //         descricao: "Qualquer coisa"
+    //     },
+    //     {
+    //         id: uuidv4(),
+    //         titulo: "Dart, o Caçador de Andróides",
+    //         linkVideo: "https://www.youtube.com/embed/LLmBWjF6F8M",
+    //         linkImagem: "https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img3.png",
+    //         categoria: "Data Science",
+    //         descricao: "Qualquer coisa"
+    //     }
+]
 
 
 export default function VideosProvider({ children }) {
     const [videos, setVideos] = useState(inicial);
-    //const [videosFiltrados, setVideosFiltrados] = useState ();
+    const endpointDaApi = 'https://651b20b1194f77f2a5ae4300.mockapi.io/Vidoes';
 
-    const adicionarVideo = (novoVideo) => {
-        setVideos([...videos, { ...novoVideo, id: uuidv4() }])
+    useEffect(() => {
+        async function listarVideos() {
+            try {
+                const resposta = await fetch(endpointDaApi);
+                if (!resposta.ok) {
+                    throw new Error('Erro ao buscar vídeos.');
+                }
+                const dados = await resposta.json();
+                setVideos(dados);
+            } catch (erro) {
+                console.error(erro);
+            }
+        }
+
+        listarVideos();
+    }, []);
+
+    async function adicionarVideo(novoVideo) {
+
+        const resposta = await fetch(endpointDaApi, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(novoVideo)
+        });
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao adicionar vídeo.');
+        }
+
+        const dados = await resposta.json();
+        setVideos([...videos, dados]);
     }
+
 
     const deletarVideo = (videoDeletado) => {
         let novaLista = [...videos];
@@ -69,5 +101,5 @@ export default function VideosProvider({ children }) {
 
 export function useVideosContext() {
     return useContext(VideosContext);
-    
+
 }
